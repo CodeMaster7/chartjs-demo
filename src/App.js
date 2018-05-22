b747dd7efad6bb9cef0ec870db04eaa0915d0a88
nathan-08 <nathanryan001@gmail.com>
2018-05-22 10:25:57 -0600

    cleanup app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Doughnut, Line, Bar, defaults } from 'react-chartjs-2';
-defaults.global.defaultFontFamily = "'Open Sans Condensed', sans-serif"; // set font family
-function getRandomInt(min, max) {
+defaults.global.defaultFontFamily = "'Open Sans Condensed', sans-serif"; // set default chart font family
+function getRandomInt(min, max) {   // function to generate random numbers within a range
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 const colors = ['#4f6d7a', '#c0d6df', '#dbe9ee', '#4a6fa5', '#166088']; // some color schemes
@@ -38,13 +38,11 @@ export default class App extends Component {
   constructor() {
     super()
     this.state = {
-      value1: 50,
+      value1: 50,   // values for Doughnut chart
       value2: 100,
       value3: 200,
-
     }
-    super()
-    setInterval(() => this.forceUpdate(), 1000 * 6)
+    setInterval(() => this.forceUpdate(), 1000 * 6) // rerender every 6 seconds, causing bar chart to generate new data
   }
   render() {
     console.log('defaults: ', defaults)
@@ -71,7 +69,7 @@ export default class App extends Component {
           <Line
             height={300}
             width={200}
-            data={getState()}
+            data={getState()} // will generate random values 
           />
         </div>
         <div className='container'>
@@ -87,18 +85,6 @@ export default class App extends Component {
             <div className='box box1' /> <input onChange={(e) => this.setState({ value1: e.target.value })} value={this.state.value1} /></div><div className='flex'>
             <div className='box box2' /><input onChange={(e) => this.setState({ value2: e.target.value })} value={this.state.value2} /></div><div className='flex'>
             <div className='box box3' /><input onChange={(e) => this.setState({ value3: e.target.value })} value={this.state.value3} /></div>
-        </div>
-        <div className='container' style={{ display: 'none' }}>
-          <label>Bar</label>
-          <Bar
-
-            data={barData}
-            height={300}
-            width={400}
-            options={{
-              maintainAspectRatio: true
-            }}
-          />
         </div>
       </div >
     );
